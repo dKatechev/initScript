9b5a91c60826ed75e961017a0715a13af628dc1c
Dzmitry Katsechau <Dzmitry_Katsechau@epam.com>
2019-11-12 10:34:38 +0300

    Combine download/install files, update Git related scripts

--- funcs/installGit.psm1
+++ funcs/installGit.psm1
@@ -1,3 +1,20 @@
+function Download-Git
+{
+	'.\*' | gci -include '*.psm1' | Import-Module
+
+	$appProps = Convertfrom-Stringdata (Get-Content ./app.properties -raw)
+	$downloadDir = [System.IO.Path]::GetFullPath($appProps.'download.dir')
+	
+	$gitVersion = $appProps.'git.version'
+	Write-Output "Git version: $gitVersion"
+	$gitDownloadUrl = "https://github.com/git-for-windows/git/releases/download/v" + $gitVersion + ".windows.2/Git-" + $gitVersion + ".2-64-bit.exe"
+
+	Write-Output "Start git downloading ..."
+	$downloadDir = $downloadDir + "\git\"
+	New-Item -Path $downloadDir -ItemType "directory" -Force
+	Download-File $gitDownloadUrl $downloadDir
+}
+
 function Install-Git
 {
 	$appProps = Convertfrom-Stringdata (Get-Content ./app.properties -raw)
@@ -5,15 +22,17 @@ function Install-Git
 	$downloadDir = $downloadDir + "\git\"
 	$installDir = [System.IO.Path]::GetFullPath($appProps.'install.dir')
 	$installDir = $installDir + "\git\"
+	Write-Output "Git install dir: $installDir"
 
 	$installer = Get-ChildItem -Path $downloadDir -Filter *.exe
 	$installer = $downloadDir + "\" + $installer
+	Write-Output "Git installer: $installer"
 
 	if(-not(Test-Path -LiteralPath $installer)) {
 		Write-Output "Git installer is not found: $installer"
 		exit
 	}
 
-	[String] $installArgs = "/S /D=" + $installDir
-	Start-Process -NoNewWindow -FilePath $installer -Args $installArgs -Verb RunAs -Wait
+	[String] $installArgs = "/VERYSILENT /DIR=" + $installDir
+	Start-Process -FilePath $installer -Args $installArgs -Verb RunAs -Wait
 }
--- funcs/installJava.psm1
+++ funcs/installJava.psm1
@@ -1,3 +1,32 @@
+function Download-Java()
+{
+	try {
+		$appProps = Convertfrom-Stringdata (Get-Content ./app.properties -raw)
+		[String] $jdkVersion = $appProps.'jdk.version'
+		[String] $jdkFullVersion = $jdkVersion + "+9"
+		[String] $jdkUrl = "http://download.oracle.com/otn-pub/java/jdk/" + $jdkFullVersion + "/cec27d702aa74d5a8630c65ae61e4305/jdk-" + $jdkVersion + "_windows-x64_bin.zip"
+		Write-Output "JDK download URL: $jdkUrl"
+		$downloadDir = [System.IO.Path]::GetFullPath($appProps.'download.dir')
+		$downloadDir = $downloadDir + "\jdk\"
+		New-Item -Path $downloadDir -ItemType "directory" -Force
+		[String] $localFile = $jdkUrl.SubString($jdkUrl.LastIndexOf('/'))
+		$localFile = $downloadDir + $localFile
+		Write-Output "JDK location: $localFile"
+
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		$client = New-Object System.Net.WebClient		
+		$cookie = "oraclelicense=accept-securebackup-cookie"
+		$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
+		Write-Progress -Activity 'Downloading file' -Status $jdkUrl
+		
+		$client.DownloadFile($jdkUrl, $localFile)
+	} finally {
+		$client.Dispose()
+		Remove-Variable client
+		[GC]::Collect()
+	}
+}
+
 function Install-Java
 {
 	'.\*' | gci -include '*.psm1' | Import-Module
